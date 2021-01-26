50c6906fccd00eb0b77e40cd4946c6536117db71
denisedaniels <denise_daniels@bloomfield.edu>
2021-01-26 15:36:20 -0500

    Image added

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.codepath.bestsellerlistapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -41,6 +42,12 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         holder.mBookAuthor.setText(books.get(position).author);
         holder.mBookDescription.setText(books.get(position).description);
 
+        BestSellerBook bestSellerBook = books.get(position);
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
+
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +71,7 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
         public final TextView mBookDescription;
+        public final ImageView mBookImage;
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
@@ -73,11 +81,12 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
             mBookDescription = (TextView) view.findViewById(R.id.book_description);
+            mBookImage=(ImageView) view.findViewById(R.id.book_image);
         }
 
         @Override
         public String toString() {
-            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'" + mBookRank.toString() + "'" + mBookDescription.getText() + "'";
+            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'" + mBookRank.toString() + "'" + mBookDescription.getText() ;
         }
     }
 }
